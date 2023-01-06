81976ea6edfddc07f472b5943dad3ef398acf132
cam <cam@one.lan>
2023-01-06 17:45:11 +0100

    first commit

---
+++ tests/src/vfs/extfs/helpers-list/data/config.sh
@@ -0,0 +1,9 @@
+#
+# Configure-time parameters that may be useful in tests.
+#
+# See README for how to use this file.
+#
+PERL="/usr/bin/perl"
+AWK="awk"
+PYTHON="/usr/bin/python"
+RUBY="/usr/bin/ruby"
